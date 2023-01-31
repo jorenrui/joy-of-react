9fdd0545d9a26474929a9e6bb905f773bd90ee4f
Joeylene <23741509+jorenrui@users.noreply.github.com>
2023-01-31 18:37:11 +0800

    feat: add guess input

---
+++ word-clone/src/components/GuessInput/GuessInput.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function GuessInput({ onSubmit }) {
+  const [guess, setGuess] = React.useState("");
+  
+  const handleChange = (evt) => {
+    setGuess(evt.target.value.toUpperCase());
+  };
+
+  const submit = (evt) => {
+    evt.preventDefault();
+
+    if (guess.length < 2) {
+      alert("Guess must be at least 2 characters long");
+      return;
+    }
+
+    if (guess.length > 5) {
+      alert("Guess must be at most 5 characters long");
+      return;
+    }
+
+    onSubmit(guess);
+    setGuess("");
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={submit}>
+      <label htmlFor="guess-input">
+        Enter guess:
+      </label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleChange}
+        minLength={2}
+        maxLength={5}
+        required
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
