891f3c882abb10b163f6a82e25259de4f0b7fac4
Joeylene <23741509+jorenrui@users.noreply.github.com>
2023-01-31 19:11:02 +0800

    feat: display game results

--- word-clone/src/components/Game/Game.js
+++ word-clone/src/components/Game/Game.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { sample } from '../../utils';
+import { range, sample } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
+import { checkGuess } from '../../game-helpers';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,12 +12,45 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  const submit = (guess) => {
-    console.log(guess);
+  const [guesses, setGuesses] = React.useState(range(0, 6).map(_ => []));
+  const [tries, setTries] = React.useState(0);
+
+  const addGuess = (guess) => {
+    if (tries >= NUM_OF_GUESSES_ALLOWED) {
+      alert("Sorry, you run out of tries. You lose.");
+      return;
+    }
+
+    const updatedGuesses = [...guesses];
+    updatedGuesses[tries] = checkGuess(guess, answer);
+    setGuesses(updatedGuesses);
+    setTries(tries + 1);
   };
 
   return (
-    <GuessInput onSubmit={submit} />
+    <>
+      <div className="guess-results">
+        {range(0, 6).map((row) => {
+          const guess = guesses[row] ?? [];
+
+          return (
+            <p key={row} className="guess">
+              {range(0, 5).map((col) => {
+                if (!guess[col])
+                  return <span key={col} className="cell" />;
+                
+                return (
+                  <span key={col} className={`cell ${guess[col].status}`}>
+                    {guess[col].letter}
+                  </span>
+                );
+              })}
+            </p>
+          );
+        })}
+      </div>
+      <GuessInput onSubmit={addGuess} />
+    </>
   );
 }
 
